3f8338f238c53ede687aabd99995b84c47d362fd
John Rodler <john@jrod.info>
2014-09-28 19:35:48 -0400

    add timer & include-replace to grunt build process

---
+++ grunt/includereplace.js
@@ -0,0 +1,12 @@
+/*global module*/
+//
+// Grunt - includereplace.js
+//
+// ==========================================================================
+
+module.exports = {
+  dev: {
+    src: 'index.html',
+    dest: 'index.html'
+  }
+};
